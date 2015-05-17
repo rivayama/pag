972ca2f0507038c1bb5a31171e2165f21b77d1fa
rivayama <tomozo16@gmail.com>
2015-05-17 17:59:51 +0900

    Use environment variable for database

--- pag/settings.py
+++ pag/settings.py
@@ -76,9 +76,7 @@ WSGI_APPLICATION = 'pag.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 import dj_database_url
-DATABASES = {'default': dj_database_url.parse(
-    'sqlite:///%s' % os.path.join(BASE_DIR, 'db.sqlite3')
-)}
+DATABASES = {'default': dj_database_url.config()}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
