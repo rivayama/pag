57439ad999b9ab4fec7e7324dd435c67fcadb58b
rivayama <tomozo16@gmail.com>
2015-05-17 17:21:54 +0900

    Redirect to index after callback, add get_projects

--- home/views.py
+++ home/views.py
@@ -10,17 +10,41 @@ token_path    = '/api/v2/oauth2/token'
 
 
 def index(request):
-    return render(request, 'home/index.html', {})
+    context = {}
+    try:
+        client = OAuth2Session(client_id, token=request.session['token'])
+        context['projects'] = client.get(projects_url(request.session['backlog_space'])).json()
+    except KeyError:
+        pass
+    return render(request, 'home/index.html', context)
 
 
 def auth(request):
     client = OAuth2Session(client_id)
-    auth_base_url = 'https://' + request.POST['space'] + '.backlog.jp' + auth_path
-    authorization_url, state = client.authorization_url(auth_base_url)
+    authorization_url, state = client.authorization_url(auth_base_url(request.POST['space']))
+    request.session['backlog_space'] = request.POST['space']
+    request.session['oauth_state'] = state
     return redirect(authorization_url)
 
 
 def callback(request):
-    context = {'auth_response': request.path}
-    return render(request, 'home/callback.html', context)
+    client = OAuth2Session(client_id, state=request.session['oauth_state'])
+    response_url = request.build_absolute_uri(request.get_full_path()).replace('http', 'https')
+    token = client.fetch_token(
+        token_url(request.session['backlog_space']),
+        client_secret=client_secret,
+        authorization_response=response_url
+    )
+    request.session['token'] = token
+    return redirect('index')
 
+
+
+def auth_base_url(space):
+    return 'https://' + space + '.backlog.jp' + auth_path
+
+def token_url(space):
+    return 'https://' + space + '.backlog.jp' + token_path
+
+def projects_url(space):
+    return 'https://' + space + '.backlog.jp' + '/api/v2/projects'
