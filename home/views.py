34618a7af783f078ef78ba7c3117812eda0ae0a9
rivayama <tomozo16@gmail.com>
2015-05-18 10:09:11 +0900

    Modify variables name

--- home/views.py
+++ home/views.py
@@ -6,24 +6,24 @@ from pag.libs.backloglib2 import Backlog
 def index(request):
     context = {}
     try:
-        client = Backlog(request.session['space'], token=request.session['token'])
-        context['projects'] = client.get_projects()
+        backlog = Backlog(request.session['space'], token=request.session['token'])
+        context['projects'] = backlog.get_projects()
     except KeyError:
         pass
     return render(request, 'home/index.html', context)
 
 
 def auth(request):
-    client = Backlog(request.POST['space'])
-    auth_url, state = client.auth_url()
+    backlog = Backlog(request.POST['space'])
+    auth_url, state = backlog.auth_url()
     request.session['space'] = request.POST['space']
     request.session['state'] = state
     return redirect(auth_url)
 
 
 def callback(request):
-    client = Backlog(request.session['space'], state=request.session['state'])
+    backlog = Backlog(request.session['space'], state=request.session['state'])
     response_uri = request.build_absolute_uri(request.get_full_path()).replace('http', 'https')
-    request.session['token'] = client.fetch_token(response_uri)
+    request.session['token'] = backlog.fetch_token(response_uri)
     return redirect('index')
 
