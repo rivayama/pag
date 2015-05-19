352a3a50cb68e42d40b60ec82c1ff4f510ca7a3c
rivayama <tomozo16@gmail.com>
2015-05-19 21:22:26 +0900

    Refresh token automatically

--- home/views.py
+++ home/views.py
@@ -6,7 +6,7 @@ from pag.libs.backloglib2 import Backlog
 def index(request):
     context = {}
     try:
-        backlog = Backlog(request.session['space'], token=request.session['token'])
+        backlog = __init_backlog(request, request.session['space'], token=request.session['token'])
         context['projects'] = backlog.get_projects()
     except KeyError:
         pass
@@ -14,7 +14,7 @@ def index(request):
 
 
 def auth(request):
-    backlog = Backlog(request.POST['space'])
+    backlog = __init_backlog(request, request.POST['space'])
     auth_url, state = backlog.auth_url()
     request.session['space'] = request.POST['space']
     request.session['state'] = state
@@ -22,8 +22,15 @@ def auth(request):
 
 
 def callback(request):
-    backlog = Backlog(request.session['space'], state=request.session['state'])
+    backlog = __init_backlog(request, request.session['space'], state=request.session['state'])
     response_uri = request.build_absolute_uri(request.get_full_path()).replace('http', 'https')
     request.session['token'] = backlog.fetch_token(response_uri)
     return redirect('index')
+
+
+# No route for this. Provate use.
+def __init_backlog(request, space, state=None, token=None):
+    def token_updater(token):
+        request.session['token'] = token
+    return Backlog(space, state=state, token=token, token_updater=token_updater)
 
--- pag/libs/backloglib2/__init__.py
+++ pag/libs/backloglib2/__init__.py
@@ -10,21 +10,43 @@ client_secret = os.environ.get('BACKLOG_CLIENT_SECRET')
 
 class Backlog():
 
-    def __init__(self, space, state=None, token=None):
-        self.client = OAuth2Session(client_id, state=state, token=token)
+    def __init__(self, space, state=None, token=None, token_updater=None):
+        self.client = OAuth2Session(client_id, state=state, token=token, token_updater=token_updater)
         self.host = 'https://' + space + '.backlog.jp'
+
+
+    def fetch(self, api):
+        try:
+            return self.client.get(api).json()
+        except:
+            # XXX なぜかTokenExpiredErrorが拾えないので、後で直す
+            self.client.token = self.refresh_token()
+            return self.fetch(api)
+
 
     def auth_url(self):
         base_url = self.host + '/OAuth2AccessRequest.action'
         return self.client.authorization_url(base_url)
 
+
     def fetch_token(self, auth_response_uri):
         return self.client.fetch_token(
             self.host + '/api/v2/oauth2/token',
             client_secret=client_secret,
-            authorization_response=auth_response_uri)
+            authorization_response=auth_response_uri
+        )
+
+
+    def refresh_token(self):
+        api = self.host + '/api/v2/oauth2/token'
+        extra = {
+            'client_id': client_id,
+            'client_secret': client_secret,
+        }
+        return self.client.refresh_token(api, **extra)
+
 
     def get_projects(self):
         api = self.host + '/api/v2/projects'
-        return self.client.get(api).json()
+        return self.fetch(api)
 
