1b15caaa5961deb5dc509d71e87eb04d62c1c3ca
rivayama <tomozo16@gmail.com>
2015-05-23 18:21:27 +0900

    Remove blank lines

--- pag/settings.py
+++ pag/settings.py
@@ -95,14 +95,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGGING = {
