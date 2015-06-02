f06665293fa9b109e3ac662d522be52de91aee8b
rivayama <tomozo16@gmail.com>
2015-06-02 19:00:57 +0900

    WIP: use bootstrap

--- home/static/home/src/scripts/main.js
+++ home/static/home/src/scripts/main.js
@@ -4,23 +4,13 @@
 var React = require('react');
 var Router = require('director').Router;
 
+var ButtonInput = require('react-bootstrap').ButtonInput;
+
 // {{{ Randing page
 var RandingPage = React.createClass({
   render: function() {
-    var paperStyle = {
-      position: 'fixed',
-      zIndex: '9999',
-      top: '20%',
-      left: '0',
-      right: '0',
-      margin: '0 auto',
-      width: '500px',
-      textAlign: 'center'
-    };
     return (
-      <div>
-        <AuthForm />
-      </div>
+      <AuthForm />
     );
   }
 });
@@ -28,55 +18,87 @@ var RandingPage = React.createClass({
 var AuthForm = React.createClass({
   render: function() {
     return (
-      <form action="/auth/" method="post">
-        http://
-        <input type="text" name="space" id="space"/>
-        .backlog.jp
-        <input type="submit" value="Go!" />
+      <form action="/auth/" method="post" className="lead">
+        <p>http://<input type="text" name="space" id="space"/>.backlog.jp</p>
+        <ButtonInput type="submit" value="Grade Your Project" bsSize="large" />
       </form>
     );
   }
 });
 // }}}
 
-// {{{ Grader page
+// {{{ Project list
 var Grader = React.createClass({
+
+  getInitialState: function() {
+    return {
+      grade: []
+    };
+  },
+
+  loadGrade: function(i) {
+    var project = this.props.data[i];
+    $.ajax({
+      url: '/api/grade/' + project.id,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        if (data.length > 0) {
+          this.setState({grade: data});
+        }
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
   render: function() {
     return (
       <div>
-        <ProjectList data={this.props.data} />
+        <ul>
+          {this.props.data.map(function(project, i) {
+            return (
+              <li>
+                <a href={'#/grade/'+project.id} key={i} onClick={this.loadGrade.bind(this, i)}>{project.name}</a>
+              </li>
+            );
+          }.bind(this))}
+        </ul>
+        <GradeList data={this.state.grade} />
       </div>
     );
   }
+
 });
-var ProjectList = React.createClass({
-  render: function() {
-    return (
-      <ul>
-        {this.props.data.map(function(result) {
-           return <ProjectItemWrapper key={result.name} data={result}/>;
-        })}
-      </ul>
-    );
-  }
-});
-var ProjectItemWrapper = React.createClass({
-  render: function() {
-    return <li><a href={'#/grade/'+this.props.data.id}>{this.props.data.name}</a></li>;
-  }
-});
+// }}}
+
+// {{{ Grade
 var GradeList = React.createClass({
+
+  propTypes: {
+    data: React.PropTypes.array.isRequired
+  },
+
+  getDefaultProps: function() {
+    return {
+      data: [{'grade': []}, {'adivice': []}]
+    }
+  },
+
   render: function() {
     console.log(this.props.data);
     return (
       <table>
-        {this.props.data.map(function(result) {
-           return <GradeItemWrapper key={result.title} data={result}/>;
+        {this.props.data[0]['grade'].map(function(grade) {
+          console.log(grade);
+          return <GradeItemWrapper key={grade.title} data={grade}/>;
         })}
       </table>
     );
   }
 });
+
 var GradeItemWrapper = React.createClass({
   render: function() {
     return (
@@ -100,7 +122,7 @@ var App = React.createClass({
       cache: false,
       success: function(data) {
         if (data.length > 0) {
-          this.setState({page: 'authorized', projects: data});
+          this.setState({page: 'project', projects: data});
         }
       }.bind(this),
       error: function(xhr, status, err) {
@@ -123,15 +145,7 @@ var App = React.createClass({
       <RandingPage /> :
       <Grader data={this.state.projects}/>
 
-    var barStyle = {
-      zIndex: '9999'
-    };
-
-    return (
-      <div className="pag">
-        {page}
-      </div>
-    );
+    return <div className="pag">{page}</div>;
   },
 
   componentDidMount: function() {
@@ -146,4 +160,7 @@ var App = React.createClass({
 });
 // }}}
 
-React.render(<App />, document.getElementById('app-conteiner'));
+React.render(
+  <App />,
+  document.getElementById('app-conteiner')
+);
