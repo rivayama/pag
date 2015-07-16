a18e0fda0c56d6f87f6c8aca4ed5b40f4f83a81f
rivayama <tomozo16@gmail.com>
2015-07-16 20:10:44 +0900

    Remove "src" files

--- src/gevent-master/benchmarks/micro_sleep.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e -x
-python -mtimeit -r 6 -s'from gevent import sleep; f = lambda : 5' 'sleep(0)'
-python -mtimeit -r 6 -s'from gevent import sleep; f = lambda : 5' 'sleep(0.00001)'
-python -mtimeit -r 6 -s'from gevent import sleep; f = lambda : 5' 'sleep(0.0001)'
-python -mtimeit -r 6 -s'from gevent import sleep; f = lambda : 5' 'sleep(0.001)'
--- src/gevent/gevent/_fileobjectcommon.py
+++
@@ -1,8 +0,0 @@
-import sys
-from gevent._socketcommon import EBADF
-
-
-cancel_wait_ex = IOError(EBADF, 'File descriptor was closed in another greenlet')
-FileObjectClosed = IOError(EBADF, 'Bad file descriptor (FileObject was closed)')
-
-PYPY = hasattr(sys, 'pypy_version_info')
