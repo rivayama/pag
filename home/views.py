0dbf91e4c7c86c704547a294d86b5c8a0c461382
rivayama <tomozo16@gmail.com>
2015-07-24 22:19:18 +0900

    Add secret url, collect.

--- home/views.py
+++ home/views.py
@@ -2,12 +2,10 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, redirect
 from pag import utils
 
+from django.http import HttpRequest
+from api.views import compute_grade
+
 def index(request):
-    try:
-        if request.session['token']:
-            pass
-    except KeyError:
-        pass
     return render(request, 'home/index.html', {})
 
 
@@ -33,4 +31,24 @@ def signout(request):
     request.session.flush()
     return redirect('index')
 
+
+def collect(request):
+    backlog = utils.backlog(request, request.session['space'], token=request.session['token'])
+    projects = backlog.get_projects().json()
+
+    data = []
+    for project in projects:
+        grade = compute_grade(request, project['id'])
+        try:
+            if grade['summary']['issue_count'] < 80: continue
+        except KeyError:
+            continue
+        d = {}
+        d['name']  = grade['summary']['project_name']
+        d['total'] = grade['summary']['point']
+        for detail in grade['detail']:
+            d[detail['title']] = detail['point']
+        data.append(d)
+
+    return render(request, 'home/collect.html', {'data': data})
 
