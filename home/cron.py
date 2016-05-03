6929b253df6c85033fe92e76362a5977ff53d4ff
rivayama <tomozo16@gmail.com>
2016-05-03 17:03:12 +0900

    Add exception handler to cron job

--- home/cron.py
+++ home/cron.py
@@ -21,7 +21,10 @@ def grade():
         print('Execute grade api for project_id %d...' % task.project_id)
         request.session = {'space': task.space, 'token': task.token}
         request.GET     = {'force': 1, 'background': 1}
-        compute_grade(request, task.project_id)
+        try:
+            compute_grade(request, task.project_id)
+        except TokenExpiredError:
+            print('Token expired.')
         task.delete()
         print('End.')
 
